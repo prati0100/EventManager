9bf68c19983f512771257510c2bf4661bcd477a8
Pratyush Yadav <pratyush.mitma1@learner.manipal.edu>
2018-03-05 23:19:30 +0530

    Added onClick for Login as Manager button in StartActivity.
    onClickLoginManager() now launches ManagerLoginActivity.

--- app/src/main/java/com/eventmanager/StartActivity.java
+++ app/src/main/java/com/eventmanager/StartActivity.java
@@ -1,13 +1,27 @@
 package com.eventmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+/** This activity is the first activity that shows up when the app is launched. It will bypass the
+ * initial screen if the user has already logged in and jump directly to the corresponding activity
+ */
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    /** Start the Manager Login Activity to prompt a manager to log in
+     *
+     * @param v The view on which this is called. In this case, a button.
+     */
+    public void onClickLoginManager(View v) {
+        Intent i = new Intent(this, ManagerLoginActivity.class);
+        startActivity(i);
     }
 }
