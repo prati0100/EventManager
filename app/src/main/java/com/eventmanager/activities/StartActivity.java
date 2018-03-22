e24d15f37e46cfee6d8136dd1671cdb79ac73a04
kunal <kunaljoshi99@outlook.com>
2018-03-23 00:12:52 +0530

    Removed database initialization.

--- app/src/main/java/com/eventmanager/activities/StartActivity.java
+++ app/src/main/java/com/eventmanager/activities/StartActivity.java
@@ -19,9 +19,6 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        //Initialize database
-        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
-                AppDatabase.class, "EventDatabase").build();
     }
 
     /** Start the Manager Login Activity to prompt a manager to log in
