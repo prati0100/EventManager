1813e970dd4b15f995cf56ae728cd3a1e910fa3f
kunal <kunaljoshi99@outlook.com>
2018-03-23 00:33:30 +0530

    Added populate methods.

--- app/src/main/java/com/eventmanager/database/entity/Event.java
+++ app/src/main/java/com/eventmanager/database/entity/Event.java
@@ -1,6 +1,5 @@
 package com.eventmanager.database.entity;
 
-import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
@@ -12,12 +11,18 @@ public class Event {
     @PrimaryKey
     public int eventID;
 
-    @ColumnInfo(name = "event_name")
     public String eventName;
 
     public String room;
 
     public String time; //takes time in format "HHMM" (24-hour)
+
+    public Event (int eventID, String eventName, String room, String time) {
+        this.eventID = eventID;
+        this.eventName = eventName;
+        this.room = room;
+        this.time = time;
+    }
 
     public void setEventID(int event_id) {
         this.eventID = event_id;
@@ -49,5 +54,14 @@ public class Event {
 
     public String getTime() {
         return time;
+    }
+
+
+    public static Event[] populateData() {
+        return new Event[] {
+                new Event(1001, "Demo_Event1", "AB1-303", "1300"),
+                new Event(1002, "Demo_Event2", "AB5-306", "0900")
+
+        };
     }
 }
