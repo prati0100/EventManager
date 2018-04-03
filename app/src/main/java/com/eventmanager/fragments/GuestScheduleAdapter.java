548466aaa805d41608dbdfb3e8b2ef6da3610f03
Pratyush Yadav <pratyush.mitma1@learner.manipal.edu>
2018-04-04 03:23:18 +0530

    Shifted functionality of GuestEventsFragment to GuestScheduleFragment.
    The GuestEventsFragment right now does what the GuestScheduleFragment is supposed to do. This commit copies the functionality of the events fragment to the schedule fragment.
    
    This change is the first of two changes. The next one will replace the functionality of the events fragment to what it is supposed to do: just display the name of the events, not their time/location.

---
+++ app/src/main/java/com/eventmanager/fragments/GuestScheduleAdapter.java
@@ -0,0 +1,81 @@
+package com.eventmanager.fragments;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+import com.eventmanager.R;
+import com.eventmanager.database.entity.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuestScheduleAdapter extends RecyclerView.Adapter<GuestScheduleAdapter.ViewHolder> {
+    private List<Event> mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView guestScheduleRowName, guestScheduleRowTime, guestScheduleRowDate, guestScheduleRowLocation;
+        public ImageView guestScheduleRowCircle;
+        public ViewHolder(View view) {
+            super(view);
+            guestScheduleRowName = view.findViewById(R.id.guest_schedule_row_name);
+            guestScheduleRowTime = view.findViewById(R.id.guest_schedule_row_time);
+            guestScheduleRowDate = view.findViewById(R.id.guest_schedule_row_date);
+            guestScheduleRowLocation = view.findViewById(R.id.guest_schedule_row_location);
+            guestScheduleRowCircle = view.findViewById(R.id.guest_schedule_row_circle);
+        }
+    }
+
+    //The dataset should be the list of Events.
+    public GuestScheduleAdapter(List<Event> list) {
+        mDataset = list;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public GuestScheduleAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                            int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.guest_schedule_row, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from the dataset at this position
+        // - replace the contents of the view with that element
+
+        Event event = mDataset.get(position);
+
+        holder.guestScheduleRowName.setText(event.getEventName());
+        holder.guestScheduleRowTime.setText(event.getEventTime());
+        holder.guestScheduleRowDate.setText(event.getEventDate());
+        holder.guestScheduleRowLocation.setText(event.getRoom());
+
+        //To generate the colors for the row circle.
+        ColorGenerator generator = ColorGenerator.MATERIAL;
+
+        //Set the first letter of the event name in the circle.
+        String letter = String.valueOf(mDataset.get(position).getEventName().charAt(0));
+
+        //Create a TextDrawable for the image background.
+        TextDrawable drawable = TextDrawable.builder().buildRound(letter, generator.getRandomColor());
+        holder.guestScheduleRowCircle.setImageDrawable(drawable);
+    }
+
+    // Return the size of the dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
--- app/src/main/java/com/eventmanager/fragments/GuestScheduleFragment.java
+++ app/src/main/java/com/eventmanager/fragments/GuestScheduleFragment.java
@@ -1,7 +1,11 @@
 package com.eventmanager.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +13,9 @@ import android.view.ViewGroup;
 import com.eventmanager.R;
 import com.eventmanager.activities.GuestActivity;
 import com.eventmanager.database.AppDatabase;
+import com.eventmanager.database.entity.Event;
+
+import java.util.List;
 
 
 /**
@@ -16,6 +23,10 @@ import com.eventmanager.database.AppDatabase;
  * {@link GuestActivity}.
  */
 public class GuestScheduleFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     private AppDatabase mDatabase;
 
     public GuestScheduleFragment() {
@@ -27,5 +38,37 @@ public class GuestScheduleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_guest_schedule, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        //Get the reference to the database object.
+        mDatabase = AppDatabase.getInstance(getActivity());
+
+        //Initialize the RecyclerView.
+        mRecyclerView = view.findViewById(R.id.guest_schedule_recycler);
+
+        //Use a linear layout manager for the recycler.
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //Improves performance when the content layout size does not change for different items.
+        mRecyclerView.setHasFixedSize(true);
+
+        //Get the list of all events. The database must not be accessed from the UI thread because
+        //it may potentially lock the UI for a long time.
+        new DatabaseTask().execute();
+
+    }
+
+    class DatabaseTask extends AsyncTask<Void, Void, List<Event>> {
+        protected List<Event> doInBackground(Void... params) {
+            return mDatabase.eventDao().getAllEvents();
+        }
+
+        protected void onPostExecute(List<Event> list) {
+            mAdapter = new GuestScheduleAdapter(list);
+            mRecyclerView.setAdapter(mAdapter);
+        }
     }
 }
