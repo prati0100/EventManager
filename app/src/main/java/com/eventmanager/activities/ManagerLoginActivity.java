467fb1b16896feb1c2656bffc04b202430e821f2
Pratyush Yadav <pratyush.mitma1@learner.manipal.edu>
2018-04-07 03:15:39 +0530

    Implemented login for managers.
    Earlier, because the database was not implemented, the login did nothing. Now it fetches the details from the database and does the login.
    
    Also, now the ManagerLoginActivity shows a toast for both successful and unsuccessful login. Added a string for successful login in strings.xml

--- app/src/main/java/com/eventmanager/activities/ManagerLoginActivity.java
+++ app/src/main/java/com/eventmanager/activities/ManagerLoginActivity.java
@@ -10,9 +10,12 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.eventmanager.R;
+import com.eventmanager.database.AppDatabase;
+import com.eventmanager.database.entity.EventHead;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+import java.util.List;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 
@@ -40,9 +43,14 @@ public class ManagerLoginActivity extends AppCompatActivity {
 
         result = doLogin(managerId, password);
 
-        //If the login fails, show an error message and
-        if(!result) {
-            Toast.makeText(this, getString(R.string.login_failed), Toast.LENGTH_LONG).show();
+        //Show a toast telling the user whether the login succeeded or not.
+        if(result) {
+            Toast.makeText(this, getString(R.string.login_successful), Toast.LENGTH_LONG)
+                    .show();
+        }
+        else {
+            Toast.makeText(this, getString(R.string.login_failed), Toast.LENGTH_LONG)
+                    .show();
             return;
         }
 
@@ -55,12 +63,10 @@ public class ManagerLoginActivity extends AppCompatActivity {
     }
 
     private boolean doLogin(String managerId, String password) {
-        //TODO: Implement this once the databases are set up
-
         //Do the credential check on a different thread.
         try {
-            LoginTask task = new LoginTask();
-            task.execute(managerId, password);
+            LoginTask task = new LoginTask(managerId, password, AppDatabase.getInstance(this));
+            task.execute();
             return task.get();
         } catch(CancellationException | InterruptedException | ExecutionException e) {
             return false;
@@ -68,27 +74,31 @@ public class ManagerLoginActivity extends AppCompatActivity {
 
     }
 
-    private static class LoginTask extends AsyncTask<String, Void, Boolean> {
-        protected Boolean doInBackground(String... params) {
-            String managerId, password, hashed;
+    private static class LoginTask extends AsyncTask<Void, Void, Boolean> {
+        private AppDatabase database;
+        private String managerId, password;
 
-            //Only the ID and password should be passed. If more or less parameters are passed,
-            //return an error
-            if(params.length != 2) {
+        public LoginTask(String managerId, String password, AppDatabase database) {
+            this.managerId = managerId;
+            this.password = password;
+            this.database = database;
+        }
+
+        protected Boolean doInBackground(Void... params) {
+            int id = Integer.parseInt(managerId);
+            String hashed;
+
+            //Get event head object from the database.
+            List<EventHead> list = database.eventDao().getEventHeadFromId(id);
+            //If the id does not exist in the database, login fails.
+            if(list.size() == 0) {
                 return false;
             }
-
-            managerId = params[0];
-            password = params[1];
-
-            //TODO: Get the hashed string from the database
-            //The hash for string "abc". Replace this value with the one retrieved from the database
-            hashed = "$2a$10$/H6mqwKWrtURpo0hZNmcw.6y1xi7Q3Fnnxwd0yE2l/ZKX5mhtV5zO";
+            hashed = list.get(0).getPassword();
 
             //Check if the password is correct
             return BCrypt.checkpw(password, hashed);
         }
-
     }
 
 }
