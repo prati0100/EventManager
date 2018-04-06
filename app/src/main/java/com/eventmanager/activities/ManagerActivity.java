bb689a0b0aa2475cc0222cd9d040c7a2388222f2
Pratyush Yadav <pratyush.mitma1@learner.manipal.edu>
2018-04-07 04:39:05 +0530

    Added ManagerEventsFragment.
    This shows all the events to the manager. Not all functionality implemented yet. Edit option and more to add.

--- app/src/main/java/com/eventmanager/activities/ManagerActivity.java
+++ app/src/main/java/com/eventmanager/activities/ManagerActivity.java
@@ -1,17 +1,23 @@
 package com.eventmanager.activities;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.eventmanager.R;
+import com.eventmanager.fragments.ManagerEventsFragment;
 
 public class ManagerActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private FragmentManager mFragmentManager;
+
+    private Fragment currentFragment;
+
+    private ManagerEventsFragment mManagerEventsFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,10 +26,19 @@ public class ManagerActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.manager_navigation_events:
-                    mTextMessage.setText(R.string.title_events);
+                    if(currentFragment == mManagerEventsFragment) {
+                        return true;
+                    }
+
+                    getSupportActionBar().setTitle(R.string.title_events);
+
+                    mFragmentManager.beginTransaction()
+                            .replace(R.id.manager_fragment_container, mManagerEventsFragment)
+                            .commit();
+                    currentFragment = mManagerEventsFragment;
                     return true;
+
                 case R.id.manager_navigation_speakers:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -35,7 +50,15 @@ public class ManagerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        mFragmentManager = getFragmentManager();
+
+        mManagerEventsFragment = new ManagerEventsFragment();
+
+        //Initially, display the events fragment.
+        currentFragment = mManagerEventsFragment;
+        mFragmentManager.beginTransaction()
+                .add(R.id.manager_fragment_container, mManagerEventsFragment).commit();
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.manager_navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
---
+++ app/src/main/java/com/eventmanager/fragments/ManagerEventsAdapter.java
@@ -0,0 +1,71 @@
+package com.eventmanager.fragments;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+import com.eventmanager.R;
+import com.eventmanager.database.entity.Event;
+
+import java.util.List;
+
+public class ManagerEventsAdapter extends RecyclerView.Adapter<ManagerEventsAdapter.ViewHolder> {
+    private List<Event> mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView managerEventRowName;
+        public ImageView managerEventRowCircle;
+        public ViewHolder(View view) {
+            super(view);
+            managerEventRowName = view.findViewById(R.id.manager_events_row_name);
+            managerEventRowCircle = view.findViewById(R.id.manager_events_row_circle);
+        }
+    }
+
+    public ManagerEventsAdapter(List<Event> list) {
+        mDataset = list;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ManagerEventsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                            int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.manager_events_row, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from the dataset at this position
+        // - replace the contents of the view with that element
+
+        Event event = mDataset.get(position);
+
+        holder.managerEventRowName.setText(event.getEventName());
+
+        //To generate the colors for the row circle.
+        ColorGenerator generator = ColorGenerator.MATERIAL;
+
+        //Set the first letter of the event name in the circle.
+        String letter = String.valueOf(mDataset.get(position).getEventName().charAt(0));
+
+        //Create a TextDrawable for the image background.
+        TextDrawable drawable = TextDrawable.builder().buildRound(letter, generator.getRandomColor());
+        holder.managerEventRowCircle.setImageDrawable(drawable);
+    }
+
+    // Return the size of the dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
