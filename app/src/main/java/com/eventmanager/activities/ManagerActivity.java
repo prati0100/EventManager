6b2bc5b3cbbdda36a01b2d0ed65e559f15a52f52
Pratyush Yadav <pratyush.mitma1@learner.manipal.edu>
2018-04-08 18:40:04 +0530

    Added option to delete the event in ManagerActivity.

--- app/src/main/java/com/eventmanager/activities/ManagerActivity.java
+++ app/src/main/java/com/eventmanager/activities/ManagerActivity.java
@@ -1,8 +1,13 @@
 package com.eventmanager.activities;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -13,11 +18,20 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.eventmanager.R;
+import com.eventmanager.database.AppDatabase;
+import com.eventmanager.database.entity.Event;
+import com.eventmanager.database.entity.EventHead;
+import com.eventmanager.database.entity.Speaker;
+import com.eventmanager.database.entity.Volunteer;
 import com.eventmanager.fragments.ManagerEventsFragment;
 import com.eventmanager.fragments.ManagerSpeakerFragment;
 
+import java.util.List;
+
 public class ManagerActivity extends AppCompatActivity {
     private static int managerId;
+
+    private boolean done;
 
     private FragmentManager mFragmentManager;
 
@@ -71,6 +85,10 @@ public class ManagerActivity extends AppCompatActivity {
                 Intent i = new Intent(this, EventCreateActivity.class);
                 startActivity(i);
                 return true;
+
+            case R.id.manager_events_menu_delete_event:
+                deleteCurrentEvent();
+                return true;
         }
         return true;
     }
@@ -114,7 +132,90 @@ public class ManagerActivity extends AppCompatActivity {
         return true;
     }
 
+    private void deleteCurrentEvent() {
+        //Show an alert to confirm user action.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.alert);
+        builder.setMessage(R.string.delete_event_prompt);
+
+        String positiveText = getString(android.R.string.ok);
+
+        AppDatabase database = AppDatabase.getInstance(getApplicationContext());
+
+        final EventDeleteTask task = new EventDeleteTask(database, this);
+        builder.setPositiveButton(positiveText, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                task.execute();
+
+                dialog.dismiss();
+            }
+        });
+
+        String negativeText = getString(android.R.string.cancel);
+        builder.setNegativeButton(negativeText, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Do nothing.
+            }
+        });
+
+        //Display the dialog.
+        builder.create().show();
+    }
+
     public static int getCurrentManagerId() {
         return managerId;
+    }
+
+    private static class EventDeleteTask extends AsyncTask<Void, Void, Void> {
+        private AppDatabase database;
+        private Context context;
+
+        public EventDeleteTask(AppDatabase database, Context context) {
+            this.database = database;
+            this.context = context;
+        }
+
+        protected Void doInBackground(Void... params) {
+            //Get the current manager and corresponding event.
+            List<EventHead> eventHeadList = database.eventDao()
+                    .getEventHeadFromId(getCurrentManagerId());
+
+            EventHead head = eventHeadList.get(0);
+
+            List<Event> eventList = database.eventDao().getEventById(head.getEventID());
+
+            Event event = eventList.get(0);
+
+            //Get the speaker for this event.
+            List<Speaker> speakerList = database.eventDao().getSpeakerFromManagerId(head.getId());
+
+            Speaker speaker = speakerList.get(0);
+
+            //Get all the volunteers for this event.
+            List<Volunteer> volunteerList = database.eventDao().getEventVolunteers(event.getEventID());
+
+            //Delete the volunteers
+            database.eventDao().deleteVolunteers(volunteerList);
+
+            //Delete the event head.
+            database.eventDao().deleteEventHead(head);
+
+            //Delete the speaker.
+            database.eventDao().deleteSpeaker(speaker);
+
+            //Delete the event.
+            database.eventDao().deleteEvent(event);
+
+            //Set the current event head id to 0.
+            managerId = 0;
+
+            return null;
+        }
+
+        protected void onPostExecute(Void v) {
+            ((Activity) context).finish();
+        }
     }
 }
